237094854ff5b3b3f202fb64b9622718833123ef
choijaewoong <jwchoi0129@gmail.com>
2015-10-14 18:13:50 +0900

    Initial commit

---
+++ app/src/main/java/com/torerov/textinputlayout/MainActivity.java
@@ -0,0 +1,20 @@
+package com.torerov.textinputlayout;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    FloatingActionButton fab;
+    View container;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        container = findViewById(R.id.container);
+        fab = (FloatingActionButton)findViewById(R.id.)
+    }
+}
